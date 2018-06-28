8ddcfe9d49bc5b5d4bfd352b0dfd15a5e0fc8aa9
TRamos5 <travis.ramos5@gmail.com>
2018-06-28 09:32:58 -0500

    fixed exercise6

--- string-exercises/exercise6.py
+++ string-exercises/exercise6.py
@@ -3,11 +3,13 @@ myLetter = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 cypher = 'lbh zhfg hayrnea jung lbh unir yrnearq'
 position = 0
 newPosition = 0
+finalString= ''
 
 for n in cypher:
     if n.isspace() == False :
         position = myLetter.index(n)
         newPosition = position - 13
         if newPosition < 0:
-            newPosition = 25 + newPosition
-        print(myLetter[newPosition])
+            newPosition = 26 + newPosition
+        finalString = finalString + myLetter[newPosition]
+print(finalString)
