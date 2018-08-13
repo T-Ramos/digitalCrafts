afb407aff1d3680b3364bb4962eaf6d34168b64f
TRamos5 <travis.ramos5@gmail.com>
2018-08-13 11:34:50 -0500

    express practice

--- node-practice/class/app.js
+++ node-practice/class/app.js
@@ -3,22 +3,38 @@ let app = express();
 
 
 app.get('/', function(request, response){
-    response.send('hello world!');
+    response.send('Hello, world!');
 })
 
 
-app.get('/about', function(request, response){
-    response.send(`<h1>a;lkfja;ldjfaiodfja</h1>, if;elkajdoijaklfjaopidjflakejfopaidjfpoiejapojflkadjfl;e`);
+app.get('/cats', function(request, response){
+    response.send(`Meow`);
 })
 
 
 
-app.get('/faq', function(request, response){
-    response.send('frequently asked questions');
+app.get('/dogs', function(request, response){
+    response.send('Woof');
 })
 
+app.get('/cats_and_dogs', function(request, response){
+    response.send('Living together');
+})
+
+app.get('/greet', function(request, response){
+    var name = request.param('name');
+    response.send(`Hello, ${name}!`);
+})
 
 //creating a server
-app.listen(3000, function(){
-    console.log('listening on port 3000');
+app.listen(8000, function(){
+    console.log('listening on port 8000');
 })
+
+app.get('/hello1', function(req, res){
+
+    var name = req.query.name || 'world';
+    var year = req.query.year;
+    res.send('Hello ' + name + "! You were born in " + (2018-year) + '.');
+    
+});
