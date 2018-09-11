db0cc51980dbc837e28902d4fa6bed0d238b2a07
TRamos5 <travis.ramos5@gmail.com>
2018-09-11 14:23:25 -0500

    memory game

---
+++ react-practice/Monday/game/memory-game/src/MemoryCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends Component {
+    render () {
+        return(
+        <div class="MemoryCard">
+            <img class="img" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt=""></img>
+        </div>
+        )
+    }
+}
+
+
+
+
+export default MemoryCard;
